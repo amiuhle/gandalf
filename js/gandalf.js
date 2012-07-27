fa8d2c187ce4007b85a8b51e1dd92f0386e609fc
Timo Uhlmann <timouhlmann@googlemail.com>
2012-07-27 03:44:13 +0200

    Added page selection (kind of)

--- js/gandalf.js
+++ js/gandalf.js
@@ -1,3 +1,12 @@
-(function(w) {
-  console.log(window.location);
+(function(undefined) {
+  var screen = /screen=([^\&]*)/.exec(window.location.search)[1]
+  switch(screen) {
+    case 'map':
+      console.log("MAP!!!");
+      map = $.ajax('map.php?e=1343353051011&480_440=1');
+      console.log('loaded', map);
+    break;
+    default:
+      console.log("Ignoring " + screen);
+  }
 })();
