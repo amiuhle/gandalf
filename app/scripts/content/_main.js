ec0fd7a28c02af37362f2a9c956232c473cb4a5f
Timo Uhlmann <t.uhlmann@zeroseven.de>
2014-07-04 09:14:37 +0200

    Started with quickbox. Might be abandoned

---
+++ app/scripts/content/_main.js
@@ -0,0 +1,18 @@
+/* globals Gandalf */
+
+'use strict';
+
+window.Gandalf = (function() {
+
+	return {
+
+	};
+
+})();
+
+(function(/*scope*/) {
+	(function(){
+		var maincell = document.querySelector('.maincell');
+		console.log('quickbox', maincell);
+	})();
+})(Gandalf);
